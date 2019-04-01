de0021c705eb22b6d5fc326e3059e63b9a6a1f6b
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-04-01 23:18:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].map {|holiday, supplies| supplies.flatten}
+  holiday_hash[:winter].map do |holiday, supplies|
+    
 end
 
 def all_supplies_in_holidays(holiday_hash)
